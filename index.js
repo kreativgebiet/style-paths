91473e551d76958d1e8419fedf66b8eb4720dc92
Dominik Schmidt <visualcake@me.com>
2015-07-31 13:44:51 +0200

    v0.0.2

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 var lib = require('bower-files')();
 var path = require('path');
 var stylePaths = module.exports = function(ext) {
-  ext = typeof(ext) !== undefined ? a : ['scss', 'sass'];
+  ext = typeof(ext) !== undefined ? ext : ['scss', 'sass'];
 
   return lib.ext(ex).files.map(function(val) {
     return path.dirname(val);
