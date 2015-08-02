fcc210f728e40d0f5a6a21593c4bc0a55b3c825f
Dominik Schmidt <visualcake@me.com>
2015-08-02 15:09:35 +0200

    fix missing styleformats

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 var lib = require('bower-files')();
 var path = require('path');
 var stylePaths = module.exports = function(ext) {
-  ext = typeof(ext) !== undefined ? ext : ['scss', 'sass'];
+  if( typeof ext === 'undefined' ) {
+    ext = ['scss', 'sass'];
+  }
 
   return lib.ext(ext).files.map(function(val) {
     return path.dirname(val);
